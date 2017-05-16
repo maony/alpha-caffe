1cc6fcae0836cdacaa14f108b13caa088a66a039
maoningyuan <maoningyuan@sensetime.com>
2017-05-16 21:39:16 +0800

    Update directory

--- _tips/create_data.sh
+++
@@ -1,9 +0,0 @@
-python ../../fixed-point-ssd-new/alpha-caffe/scripts/create_annoset.py --anno-type=detection \
-    --label-type=txt --label-map-file=./labelmap_face.prototxt \
-    --check-label --shuffle \
-    ./ \
-    ./test_label.txt \
-    ./umdface_test_lmdb .
-
-# --max-dim=0 --resize-width=0 --resize-height=0 --check-label --shuffle \
-# --encode-type=jpg --encoded ./ \
--- _tips/get_test_size.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-../../fixed-point-ssd-new/alpha-caffe/build/tools/get_image_size \
-        ./ \
-        ./test_label.txt \
-        ./test_name_size.txt
-
